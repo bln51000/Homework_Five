c9422f9d7b9b04bd3d1f587cd3d0ed65e30d31e5
bln51000 <shanegibson1221@gmail.com>
2024-11-03 17:41:01 -0500

    Initial commit

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    //Gives reference to the target. we are the target to the enemy
+    [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 10f;//Define the distance between the attacker and the player
+
+    NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();//gives reference to NavMeshAgent
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //compute distance between two game objects
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if(distanceToTarget <= chaseRange)
+        {
+            navMeshAgent.SetDestination(target.position);
+            //SetDestination tells it where to go. And we want it to go
+            //to the target. Position tells where a certain game object is
+            //Telling navMeshAgent to go find the player
+        }
+    }
+}
